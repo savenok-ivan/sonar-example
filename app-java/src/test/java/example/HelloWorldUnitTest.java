0f9575b2fac8ad3d3f7f4c250f9b19f4d8e495b6
isavenok <savenok.ivan@gmail.com>
2018-03-26 12:55:55 +0300

    rolback

--- app-java/src/test/java/example/HelloWorldUnitTest.java
+++ app-java/src/test/java/example/HelloWorldUnitTest.java
@@ -4,19 +4,9 @@ import org.junit.Test;
 
 public class HelloWorldUnitTest {
 
-    @Test
-    public void test() {
-        new HelloWorld().coveredByUnitTest();
-    }
-
-    @Test
-    public void test1() {
-        new HelloWorld().coveredByIntegrationTest();
-    }
-
-    @Test
-    public void test2() {
-        new HelloWorld().notCovered();
-    }
+  @Test
+  public void test() {
+    new HelloWorld().coveredByUnitTest();
+  }
 
 }
