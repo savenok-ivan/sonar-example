d0ebfb5820997873143f8b732195b55a1881d13f
isavenok <savenok.ivan@gmail.com>
2018-03-26 12:42:14 +0300

    add tests

--- app-java/src/test/java/example/HelloWorldUnitTest.java
+++ app-java/src/test/java/example/HelloWorldUnitTest.java
@@ -1,20 +1,22 @@
 package example;
 
+import org.junit.Test;
+
 public class HelloWorldUnitTest {
 
-/*  @Test
-  public void test() {
-    new HelloWorld().coveredByUnitTest();
-  }
+    @Test
+    public void test() {
+        new HelloWorld().coveredByUnitTest();
+    }
 
-  @Test
-  public void test1() {
-    new HelloWorld().coveredByIntegrationTest();
-  }
+    @Test
+    public void test1() {
+        new HelloWorld().coveredByIntegrationTest();
+    }
 
-  @Test
-  public void test2() {
-    new HelloWorld().notCovered();
-  }*/
+    @Test
+    public void test2() {
+        new HelloWorld().notCovered();
+    }
 
 }
